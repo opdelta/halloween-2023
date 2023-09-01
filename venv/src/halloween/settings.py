523452aee5827740149cd1828ac4eaf19d515b5b
opdelta <1195214+opdelta@users.noreply.github.com>
2023-09-01 13:52:14 -0400

    Postgres docker

--- venv/src/halloween/settings.py
+++ venv/src/halloween/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'halloween.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': Path.joinpath(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
