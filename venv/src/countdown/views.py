e9ca7e4ee110e028c6390d48b8afc6bb4a469faf
Ziad Lteif <cj291059>
2023-10-14 12:44:23 -0400

    Default tag fix

--- venv/src/countdown/views.py
+++ venv/src/countdown/views.py
@@ -118,7 +118,8 @@ def show_message(request, message_id):
     print("Fetched message[" + str(message_id) + "] from database: " + str(message))
     # if a message has been seen, show message id 0. However, if it removes time, the time still gets removed. If it adds time, time does not get added again.
     if message_id == 2:
-        return render(request, 'message.html', {'message': message})
+        print("Message id 2 detected.")
+        return render(request, 'message.html', {'message': message.message})
     if been_seen == True:
         message = get_object_or_404(Message, id=1)
         if api_endpoint == 2:
